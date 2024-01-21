94cb514739354c8d03b46a497063118853f9bd31
mjavadl64 <laraibe@gmail.com>
2024-01-21 18:01:45 +0100

    Update ServerFTP.java

--- TP1/src/ServerFTP.java
+++ TP1/src/ServerFTP.java
@@ -9,12 +9,11 @@ import java.util.Scanner;
 
 public class ServerFTP {
     private static int myPort = 2121, myPortData = 2024;
-    private static ServerSocket myServer,myServerData;
-    private static Socket myClient,myClientData;
-    private static Boolean chek = true; 
+    private static ServerSocket myServer, myServerData;
+    private static Socket myClient, myClientData;
+    private static Boolean isConnecte = true;
 
-    private static void start () throws IOException
-    {
+    private static void start() throws IOException {
 
         String msgPassive = "229 Entering Extended Passive Mode (|||2024|)\r\n";
         String msgAccepDateConnection = "150 Accept data connection \r\n";
@@ -22,6 +21,7 @@ public class ServerFTP {
         String msgUnix = "215 UNIX Type: L8\r\n";
         String msgNotLogedin = "430 Try again:\r\n";
         String msgErrore = "501 Error, try again:\r\n";
+        String msgWindonw = "200 accepted\r\n";
         String msgTypeAccept = "200 Type accepted\r\n";
         String msqLogOut = "231 You are loged out\r\n";
         String msgUsrName = "220 Enter your username:\r\n";
@@ -30,9 +30,9 @@ public class ServerFTP {
         String msgUsrValid = "331 User Valid\r\n";
         String passWord = "123456";
         String usrName = "javad";
-        String pass,usr,quite,str,input,fileName;
+        String str, message, fileName;
 
-        //donner un port à mon server 
+        // donner un port à mon server
         myServer = new ServerSocket(myPort);
         System.out.println("Ready to connect...");
 
@@ -40,94 +40,90 @@ public class ServerFTP {
         myClient = myServer.accept();
         System.out.println("We are connected with port 2121");
 
-        while (chek) {
-           
-        //Message sur client pour demander identifiant
         OutputStream out = myClient.getOutputStream();
+        InputStream in = myClient.getInputStream();
+        Scanner scanner = new Scanner(in);
+
+        // Message sur client pour demander identifiant
         out.write(msgUsrName.getBytes());
 
-        //Recevoir la reponce d'identifiant de client 
-        InputStream in = myClient.getInputStream();
-        Scanner scanner = new Scanner(in);              
-        usr = scanner.nextLine();
+        // Recevoir la reponse de client
+        str = scanner.nextLine();
 
-        //Verifier identifiant
-            if (usr.equals("USER "+usrName)){
-                System.out.println("Welcom "+ usrName );
-                out.write(msgUsrValid.getBytes());  
-                      
-            }
-            else {
-                System.out.println(usr);
-                System.out.println("Your Username is incorrect, try again:");
-                out.write(msgNotLogedin.getBytes());
-            }
-        //Recupere mot de pass
-            pass = scanner.nextLine();
-            
-        //Verifier mot de pass
-            if (pass.equals("PASS "+passWord)) {
-                System.out.println("You are loge in");
-                out.write(msgPassIsValid.getBytes());
-                System.out.println(pass);              
-            }             
-            else {
-                out.write(msgNotLogedin.getBytes());
-                System.out.println(pass);
-                System.out.println("Your password is incorrect");
-            }
-            
+        // traiter la reponse
+
+        if (str.equals("OPTS UTF8 ON")) {
+            out.write(msgWindonw.getBytes());
+        }
+
+        // Verifier identifiant
+        str = scanner.nextLine();
+        if (str.equals("USER " + usrName)) {
+            System.out.println("Welcom " + usrName);
+            out.write(msgUsrValid.getBytes());
+
+        } else {
+            System.out.println(str);
+            System.out.println("Your Username is incorrect, try again:");
+            out.write(msgNotLogedin.getBytes());
+        }
+
+        // Verifier mot de pass
+        str = scanner.nextLine();
+        if (str.equals("PASS " + passWord)) {
+            System.out.println("You are loge in");
+            out.write(msgPassIsValid.getBytes());
+            System.out.println(str);
+        } else {
+            out.write(msgNotLogedin.getBytes());
+            System.out.println(str);
+            System.out.println("Your password is incorrect");
+        }
+
+        while (isConnecte) {
+
             str = scanner.nextLine();
-            if (str.equals("SYST")){
+            // spliter le message
+            message = str.split("\\s+")[0];
+            fileName = str.split("\\s+")[1];
+
+            if (message.equals("SYST")) {
                 out.write(msgUnix.getBytes());
             }
-            
-            str = scanner.nextLine();
-            if (str.equals("FEAT")){
+
+            if (message.equals("FEAT")) {
                 out.write(msgFeatures.getBytes());
             }
 
-            str = scanner.nextLine();
-            if (str.equals("TYPE I")){
+            if (message.equals("TYPE")) {
                 out.write(msgTypeAccept.getBytes());
                 System.out.println(str);
             }
 
-            
-            str = scanner.nextLine();
-            if (str.equals("EPSV")){
+            if (message.equals("EPSV")) {
                 System.out.println(str);
                 out.write(msgPassive.getBytes());
                 // Cree nouvaue port
                 myServerData = new ServerSocket(myPortData);
-                
-              
+
             }
-            //split le message de client
-            str = scanner.nextLine();
-            input = str.split("\\s+")[0];
-            fileName = str.split("\\s+")[1];
 
+            if (message.equals("RETR")) {
 
-            if (input.equals("RETR")){
-
-                myClientData = new Socket();
                 myClientData = myServerData.accept();
-
                 out.write(msgAccepDateConnection.getBytes());
                 System.out.println("connected to new port");
-                
-                // recupere le fichier 
-                OutputStream outPut = myClientData.getOutputStream() ;
 
-                //File file = new File("image/"+fileName);
+                // recupere le fichier:
+
+                OutputStream outPut = myClientData.getOutputStream();
+                // File file = new File("image/"+fileName);
                 InputStream filInput = new FileInputStream("src/image/proprioter.jpg");
-      
 
                 byte[] buffer = new byte[1024];
                 int bytesRead;
-                while ((bytesRead = filInput.read(buffer)) != -1){
-                    outPut.write(buffer,0,bytesRead);
+                while ((bytesRead = filInput.read(buffer)) != -1) {
+                    outPut.write(buffer, 0, bytesRead);
                 }
 
                 outPut.close();
@@ -136,39 +132,19 @@ public class ServerFTP {
 
                 out.write(msgCloseServerSocket.getBytes());
 
-                
-                
-                
             }
 
-            str = scanner.nextLine();
-            if (str.equals("QUIT")){
-                System.out.println(str);
+            if (message.equals("QUIT")) {
+                System.out.println(message);
                 out.write(msqLogOut.getBytes());
                 scanner.close();
-                chek = false;
+                isConnecte = false;
             }
-            
-            //
         }
-        
 
-        /*//Bouqule d'attendre de tapper quite    
-        while(chek) {
-            quite = scanner.nextLine();
-            if (quite.equals("QUIT")){
-                out.write(msqLogOut.getBytes());
-                chek = false;
-            }
-        */
+    }
 
-        
-    
-        }   
-
-    
-
-     public static void main(String[] args) throws IOException{
-    start();
-   }
+    public static void main(String[] args) throws IOException {
+        start();
+    }
 }
